5fbf6e443369c3f37656188236c18238648e089d
xaviguasch <xaviguasch@gmail.com>
2018-11-17 13:38:33 +0100

    adding redux part 2

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,55 @@
 import { createStore } from 'redux'
 
-const store = createStore((state = { count: 0 }) => {
-    return state
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            }
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state
+    }    
 })
 
 console.log(store.getState())
+
+
+
+
+
+// Actions - it's an object that gets send to the store
+
+// Increment the count 
+store.dispatch({
+    type: 'INCREMENT'
+})
+store.dispatch({
+    type: 'INCREMENT'
+})
+
+
+// Reset the count to zero
+store.dispatch({
+    type: 'RESET'
+})
+
+// Decrement the count
+store.dispatch({
+    type: 'DECREMENT'
+})
+
+
+// Whenever store dispatch gets called, createStore gets called to. Then depending on the dispatched action,
+// the count changes.
+
+
+console.log(store.getState()) // It gets {count: -1}    We incremented twice (+2), reseted (0) and decremented
+// (-1).
