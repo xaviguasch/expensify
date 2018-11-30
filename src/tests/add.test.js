48309a7e9a5183a7e50b27a26eba2b48e3f8edcd
xaviguasch <xaviguasch@gmail.com>
2018-11-30 20:04:22 +0100

    adding tests with Jest

---
+++ src/tests/add.test.js
@@ -0,0 +1,26 @@
+const add = (a, b) => a + b 
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`
+
+
+
+
+test('should add two numbers', () => {
+    const result = add(3, 4)
+
+    expect(result).toBe(7)
+
+  
+})
+
+
+test('should generate greeting from name', () => {
+    const returned = generateGreeting('Xavi')
+
+    expect(returned).toBe('Hello Xavi!')
+})
+
+test('should generate greeting for no name', () => {
+    const result = generateGreeting()
+
+    expect(result).toBe('Hello Anonymous!')
+})
